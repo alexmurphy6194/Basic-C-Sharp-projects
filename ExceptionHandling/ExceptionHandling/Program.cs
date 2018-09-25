6103b6d816ed54d4338f53a423e2a5e3a075d246
Alex Murphy <alexmurphy6194@gmail.com>
2018-09-25 13:16:22 -0700

    Exception Handling Drill

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        List<int> ints = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        Console.WriteLine("Please enter a number to divide by");
+        int user = Convert.ToInt32(Console.ReadLine());
+        foreach (int num in ints)
+        {
+            Console.WriteLine(num / user);
+        }
+        try
+        {
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two....");
+            int num3 = num1 / num2;
+            Console.WriteLine("The first number " + num1 + " divided by the second number " + num2 + " equals " + num3);
+            
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (DivideByZeroException dv)
+        {
+            Console.WriteLine("Please do not divide by zero");
+        }
+        catch (Exception general)
+        {
+            Console.WriteLine(general);
+
+        }
+        finally  {
+            Console.Read();
+        }
+        
+    }
+    }
+
---
+++ ExceptionHandling/ExceptionsDrill/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> ints = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        Console.WriteLine("Please enter a number to divide by");
+        try
+        {
+            int user = Convert.ToInt32(Console.ReadLine());
+            foreach (int num in ints)
+            {
+                Console.WriteLine(num / user);
+            }
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please only type numbers");
+        }
+        catch (DivideByZeroException dv)
+        {
+            Console.WriteLine("Please do not divide by zero");
+        }
+        catch (Exception general)
+        {
+            Console.WriteLine(general);
+
+        }
+        Console.WriteLine("You are now outside of the Try/Catch block");
+        Console.Read();
+    }
+}
+
