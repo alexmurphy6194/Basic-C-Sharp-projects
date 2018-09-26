0bb5716dcc64443655ee583e8a70fd056f2a1a57
Alex Murphy <alexmurphy6194@gmail.com>
2018-09-26 11:22:33 -0700

    Method Exercise

---
+++ Method Exercise/Method Exercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Method_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please enter a number you want the Operations performed on");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Thread.Sleep(1000);
+
+            Console.WriteLine("First we will determine if your number is Even or Odd....");
+            Thread.Sleep(1000);
+            string isEven = arithmetic.isEven(num);
+            Console.WriteLine(isEven);
+
+            Console.WriteLine("Next we will see what your number is squared...");
+            Thread.Sleep(1000);
+            int squared = arithmetic.squared(num);
+            Console.WriteLine(squared);
+
+            Console.WriteLine("Then we will see what your number is + 1.....");
+            Thread.Sleep(1000);
+            int plusOne = arithmetic.plusOne(num);
+            Console.WriteLine(plusOne);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Method Exercise/Method Exercise/arithmetic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Exercise
+{
+    public static class arithmetic
+    {
+        public static string isEven(int input)
+        {
+
+            string result = "";
+            result = input % 2 == 0 ? "Even" : "Odd";
+            return result;
+        }
+
+        public static int squared(int input1)
+        {
+
+            int output = input1 * input1;
+            return output;
+        }
+
+        public static int plusOne(int input2)
+        {
+
+            int output1 = input2+1;
+            return output1;
+        }
+    }
+}
