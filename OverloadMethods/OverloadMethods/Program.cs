48431c2593309e4651c0a67c84b577236c2f2d92
Alex Murphy <alexmurphy6194@gmail.com>
2018-09-27 10:07:50 -0700

    Overload Methods Exercise

---
+++ OverloadMethods/OverloadMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations forInt = new Operations();
+            Console.WriteLine(forInt.sameMethod(12));
+            
+
+            Operations forDec = new Operations();
+            Console.WriteLine(forDec.sameMethod(1.50234934m));
+
+            Operations forString = new Operations();
+            Console.WriteLine(forString.sameMethod("50"));
+
+            Console.Read();
+        }
+    }
+}
---
+++ OverloadMethods/OverloadMethods/class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethods
+{
+    public class Operations
+    {
+        public int sameMethod(int num1)
+        {
+            num1++;
+            return num1;
+        }
+        public decimal sameMethod(decimal num1)
+        {
+            num1 = num1 / 2;
+            return num1;
+        }
+        public int sameMethod(string words)
+        {
+            int num = Convert.ToInt32(words);
+            num = num * 3; 
+            return num;
+        }
+    }
+}
