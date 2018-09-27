2d0244a409cf5766e03c5dab38ce87e683c6b9e2
Alex Murphy <alexmurphy6194@gmail.com>
2018-09-27 11:33:14 -0700

    Optional Parameters

---
+++ optionalParameters/optionalParameters/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParameters
+{
+    public class doMath
+    {
+        public int optionalNum(int num1, int multiplier = 1){
+            num1 = (num1 + 13)*multiplier;
+        return num1;
+        }
+    }
+}
---
+++ optionalParameters/optionalParameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doMath num = new doMath();
+
+            Console.WriteLine("Enter a number");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number... Or don't");
+            string words = Console.ReadLine();
+
+            int result;
+            int input2;
+
+            if (int.TryParse(words, out input2))
+            {
+                result = num.optionalNum(input1, input2);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                result = num.optionalNum(input1);
+                Console.WriteLine(result);
+            }
+            Console.Read();
+        }
+    }
+}
