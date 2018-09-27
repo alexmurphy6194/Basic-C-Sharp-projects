21a0c0172a8a994ef44c4a9217dac8245d37e639
Alex Murphy <alexmurphy6194@gmail.com>
2018-09-27 09:37:40 -0700

    Method/Class Exercise

---
+++ MethodExercise2/MethodExercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            practiceClass sample = new practiceClass();
+
+            practiceClass.mathOperation(10, 50);
+            practiceClass.mathOperation(num1: 13, num2: 11);
+            Console.Read();
+        }
+    }
+}
