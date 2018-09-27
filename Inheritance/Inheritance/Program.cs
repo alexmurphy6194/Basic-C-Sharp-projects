22fd61c06595c5b59df61a33ab560ef069421452
Alex Murphy <alexmurphy6194@gmail.com>
2018-09-27 13:54:03 -0700

    Inheritance Drill

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee employee1 = new employee() { FirstName = "Sample", LastName = "Student" };
+            employee1.SayName();
+            Console.Read();
+        }
+    }
+}
