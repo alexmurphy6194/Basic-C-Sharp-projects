4d6ea99bfeb9c6dcd0f814900cb4754569aa6772
Alex Murphy <alexmurphy6194@gmail.com>
2018-10-01 10:41:23 -0700

    Abstract class drill

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { firstName = "bob", lastName = "murphy" };
+            employee.sayName();
+            Console.Read();
+        }
+    }
+}
