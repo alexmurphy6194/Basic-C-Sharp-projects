e8b1fab67649387a032c3c07f2a2600a1a32e961
Alex Murphy <alexmurphy6194@gmail.com>
2018-10-01 11:02:57 -0700

    Interface Drill

---
+++ Interface/AbstractClass/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void sayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
