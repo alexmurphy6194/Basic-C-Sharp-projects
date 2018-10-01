7618f193acbbfc951d640113e2cb94047421ba96
Alex Murphy <alexmurphy6194@gmail.com>
2018-10-01 12:57:27 -0700

    Operator Overload Drill

---
+++ Operand overLoad/Inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee1 = new Employee() { FirstName = "Sample", LastName = "Student" , Id = 1};
+            Employee Employee2 = new Employee() { FirstName = "Alex", LastName="Murphy",Id = 2 };
+            bool same = Employee1 == Employee2;
+            Employee1.SayName();
+            Console.WriteLine("And");
+            Employee2.SayName();
+            Console.WriteLine("Are the same employee?.....Looking at their ID's, this is " + same);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Operand overLoad/Inheritance/employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        
+        public static bool operator== (Employee person1, Employee person2)
+        {
+            bool same = false;
+            if (person1.Id == person2.Id)
+            {
+                same = true;
+            }
+            return (same);
+        }
+        
+        public static bool operator!=(Employee person1, Employee person2)
+        {
+            bool same = true;
+            if(person1.Id == person2.Id)
+            {
+                same = false;
+            }
+            return (same);
+        }
+    }
+}
