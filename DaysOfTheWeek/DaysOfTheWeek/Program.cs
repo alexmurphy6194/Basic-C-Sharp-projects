a1ee1e34b17be6f2a45ab3d2df778a12e29ea202
Alex Murphy <alexmurphy6194@gmail.com>
2018-10-02 10:13:51 -0700

    Day of the week drill
    Enums

---
+++ DaysOfTheWeek/DaysOfTheWeek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the current day of the Week...");
+            string day = Console.ReadLine();
+
+            try
+            {
+                Enum.Parse(typeof(daysOfTheWeek), day);
+            }
+            catch
+            {
+                Console.WriteLine("Please Enter an actual Day of the Week.. First letter capital");
+            }
+            
+            Console.Read();
+            
+        }
+
+            public enum daysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+      }
+}
