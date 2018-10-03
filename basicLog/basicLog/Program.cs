954ad4c7c033ac0da2a23068518831fe7a32446f
Alex Murphy <alexmurphy6194@gmail.com>
2018-10-03 11:21:53 -0700

    basic log drill

---
+++ basicLog/basicLog/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace basicLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a Number");
+            string num = Console.ReadLine();
+            File.WriteAllText(@"..\log.txt", num);
+            string readBack = File.ReadAllText(@"..\log.txt");
+            Console.WriteLine(readBack);
+            Console.Read();
+
+        }
+    }
+}
