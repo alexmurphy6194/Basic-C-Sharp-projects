52d3008127412afcf553b5a3a6ca3889d0b41239
Alex Murphy <alexmurphy6194@gmail.com>
2018-10-03 11:40:31 -0700

    DateTime drill

---
+++ DateTime drill/DateTime drill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime itIs = new DateTime();
+            DateTime itWillbe = new DateTime();
+            itIs = DateTime.Now;
+            itWillbe = itIs.AddHours(hours);
+            Console.WriteLine(itIs);
+            Console.WriteLine("It will be {0} in {1} hours", itWillbe, hours);
+            Console.Read();
+        }
+    }
+}
