73f195611033f3bb868bc10057122cf001f4d74f
Alex Murphy <alexmurphy6194@gmail.com>
2018-10-03 13:44:03 -0700

    Additional features
    such as chained constructors and constants, create a variable using the keyword "var".

---
+++ AdditionalFeatures/AdditionalFeatures/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string cantTouchThis = "Try me";
+            Console.WriteLine(cantTouchThis);
+            var newPerson = new person("alex");
+            var newPerson2 = new person("bob", "murphy");
+
+            Console.Read();
+        }
+    }
+}
---
+++ AdditionalFeatures/AdditionalFeatures/person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    public class person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public person(string FName) : this(FName, "")
+        {
+
+        }
+        public person(string FName, string LName)
+        {
+            firstName = FName;
+            lastName = LName;
+        }
+        
+    }
+}
